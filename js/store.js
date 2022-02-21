2cbedb4ec292cf32a9546a0b7e1b02c4a8200662
simonbrest <brestsimon9@gmail.com>
2022-02-21 19:08:03 -0300

    actualizacion

--- js/store.js
+++ js/store.js
@@ -1,17 +1,17 @@
 const productos = [
-    { id: "1", nombre: "LAMBORGHINI HURACAN STO", class: 1, detalle: "Lamborghini Huracan STO is available right now!", categoria: "Deportivo", precio: "650.000", img: "/imagenesjs/huracan.png" },
-    { id: "2", nombre: "Lamborghini Huracan EVO RWD Spyder", class: 1, detalle: "Lamborghini Huracan EVO RWD Spyder is available right now!", categoria: "Deportivo", precio: "750.000", img: "/imagenesjs/huracanAzul.png" },
-    { id: "4", nombre: "Lamborghini Huracan EVO Spyder", class: 1, detalle: "Lamborghini Huracan EVO Spyder is available right now! ", categoria: "Deportivo", precio: "880.000", img: "/imagenesjs/huracanblanco.png" },
-    { id: "3", nombre: "Lamborghini Huracan EVO RWD", class: 1, detalle: "Lamborghini Huracan EVO RWD is available right now! ", categoria: "Deportivo", precio: "850.500", img: "/imagenesjs/huracanAmarillo.png" },
+    { id: "1", nombre: "LAMBORGHINI HURACAN STO", class: 1, detalle: "Lamborghini Huracan STO is available right now!", categoria: "Deportivo", precio: "650.000", img: "../imagenesjs/huracan.png" },
+    { id: "2", nombre: "Lamborghini Huracan EVO RWD Spyder", class: 1, detalle: "Lamborghini Huracan EVO RWD Spyder is available right now!", categoria: "Deportivo", precio: "750.000", img: "../imagenesjs/huracanAzul.png" },
+    { id: "4", nombre: "Lamborghini Huracan EVO Spyder", class: 1, detalle: "Lamborghini Huracan EVO Spyder is available right now! ", categoria: "Deportivo", precio: "880.000", img: "../imagenesjs/huracanblanco.png" },
+    { id: "3", nombre: "Lamborghini Huracan EVO RWD", class: 1, detalle: "Lamborghini Huracan EVO RWD is available right now! ", categoria: "Deportivo", precio: "850.500", img: "../imagenesjs/huracanAmarillo.png" },
     
-    { id: "5", nombre: "Lamborghini Huracan EVO", class: 1, detalle: "Lamborghini Huracan EVO is available right now!", categoria: "Deportivo", precio: "600.000", img: "/imagenesjs/huracanVerde.png" },
+    { id: "5", nombre: "Lamborghini Huracan EVO", class: 1, detalle: "Lamborghini Huracan EVO is available right now!", categoria: "Deportivo", precio: "600.000", img: "../imagenesjs/huracanVerde.png" },
 
-    { id: "6", nombre: "Lamborghini Aventador Ultimae Roadster", class: 2, detalle: "Lamborghini Aventador Ultimae Roadster is available right now!", categoria: "Familiar", precio: "1.500.000", img: "/imagenesjs/aventadorAzul.png" },
-    { id: "7", nombre: "Lamborghini Aventador Ultimae", class: 2, detalle: "Lamborghini Aventador Ultimae is available right now!", categoria: "Familiar", precio: "1.000.000", img: "/imagenesjs/aventadorGris.png" },
+    { id: "6", nombre: "Lamborghini Aventador Ultimae Roadster", class: 2, detalle: "Lamborghini Aventador Ultimae Roadster is available right now!", categoria: "Familiar", precio: "1.500.000", img: "../imagenesjs/aventadorAzul.png" },
+    { id: "7", nombre: "Lamborghini Aventador Ultimae", class: 2, detalle: "Lamborghini Aventador Ultimae is available right now!", categoria: "Familiar", precio: "1.000.000", img: "../imagenesjs/aventadorGris.png" },
 
     { id: "8", nombre: "Lamborghini URUS", class: 3, detalle: "Lamborghini URUS is available right now!", categoria: "Familiar", precio: "850.000", img: "/imagenesjs/urusamarilla.png" },
-    { id: "9", nombre: "Lamborghini URUS Roadster", class: 3, detalle: "Lamborghini URUS is available right now!", categoria: "Familiar", precio: "850.000", img: "/imagenesjs/urusnegra.png" },
-    { id: "10", nombre: "Lamborghini URUS Pear Capsule", class: 3, detalle: "Lamborghini URUS is available right now!", categoria: "Familiar", precio: "870.000", img: "/imagenesjs/urusverde.png" }
+    { id: "9", nombre: "Lamborghini URUS Roadster", class: 3, detalle: "Lamborghini URUS is available right now!", categoria: "Familiar", precio: "850.000", img: "../imagenesjs/urusnegra.png" },
+    { id: "10", nombre: "Lamborghini URUS Pear Capsule", class: 3, detalle: "Lamborghini URUS is available right now!", categoria: "Familiar", precio: "870.000", img: "../imagenesjs/urusverde.png" }
     
 ];
 
@@ -98,67 +98,67 @@ URUS();
 
 /*  EFECTOS CARDS */
 
-$("#cards").prepend('<div class="imagen-cards"><img src="/imagenesjs/sto_monza_milano.jpg" alt=""></div>');
+$("#cards").prepend('<div class="imagen-cards"><img src="../imagenesjs/sto_monza_milano.jpg" alt=""></div>');
 $(".imagen-cards").hide(); 
 $(".imagen-cards").delay(1); 
 $(".imagen-cards").slideDown(3500);  
 
-$("#cards").prepend('<div class="imagen-cards2"><img src="/imagenesjs/contenedor6.png" alt=""></div>');
+$("#cards").prepend('<div class="imagen-cards2"><img src="../imagenesjs/contenedor6.png" alt=""></div>');
 $(".imagen-cards2").hide(); 
 $(".imagen-cards2").delay(1); 
 $(".imagen-cards2").slideDown(5000);  
 
-$("#cards").prepend('<div class="imagen-cards3"><img src="/imagenesjs/contenedor11.png" alt=""></div>');
+$("#cards").prepend('<div class="imagen-cards3"><img src="../imagenesjs/contenedor11.png" alt=""></div>');
 $(".imagen-cards3").hide(); 
 $(".imagen-cards3").delay(1); 
 $(".imagen-cards3").slideDown(6700);  
 
-$("#cards").prepend('<div class="imagen-cards4"><img src="/imagenesjs/contenedor13.png" alt=""></div>');
+$("#cards").prepend('<div class="imagen-cards4"><img src="../imagenesjs/contenedor13.png" alt=""></div>');
 $(".imagen-cards4").hide(); 
 $(".imagen-cards4").delay(1); 
 $(".imagen-cards4").slideDown(9500); 
 
-$("#cards").prepend('<div class="imagen-cards5"><img src="/imagenesjs/contenedor12.png" alt=""></div>');
+$("#cards").prepend('<div class="imagen-cards5"><img src="../imagenesjs/contenedor12.png" alt=""></div>');
 $(".imagen-cards5").hide(); 
 $(".imagen-cards5").delay(1); 
 $(".imagen-cards5").slideDown(11500);
 
 /*  EFECTOS CARDS 1 */
 
-$("#cards1").prepend('<div class="imagen-cards6"><img src="/imagenesjs/contenedor17.png" alt=""></div>');
+$("#cards1").prepend('<div class="imagen-cards6"><img src="../imagenesjs/contenedor17.png" alt=""></div>');
 $(".imagen-cards6").hide(); 
 $(".imagen-cards6").delay(1); 
 $(".imagen-cards6").slideDown(15500); 
 
-$("#cards1").prepend('<div class="imagen-cards7"><img src="/imagenesjs/contenedor14.png" alt=""></div>');
+$("#cards1").prepend('<div class="imagen-cards7"><img src="../imagenesjs/contenedor14.png" alt=""></div>');
 $(".imagen-cards7").hide(); 
 $(".imagen-cards7").delay(1); 
 $(".imagen-cards7").slideDown(16500);
 
 /*  EFECTOS CARDS 2 */
 
-$("#cards2").prepend('<div class="imagen-cards8"><img src="/imagenesjs/contenedor4.jpg" alt=""></div>');
+$("#cards2").prepend('<div class="imagen-cards8"><img src="../imagenesjs/contenedor4.jpg" alt=""></div>');
 $(".imagen-cards8").hide(); 
 $(".imagen-cards8").delay(1); 
 $(".imagen-cards8").slideDown(17500); 
 
-$("#cards2").prepend('<div class="imagen-cards9"><img src="/imagenesjs/contenedor19.jpg" alt=""></div>');
+$("#cards2").prepend('<div class="imagen-cards9"><img src="../imagenesjs/contenedor19.jpg" alt=""></div>');
 $(".imagen-cards9").hide(); 
 $(".imagen-cards9").delay(1); 
 $(".imagen-cards9").slideDown(18000);
 
-$("#cards2").prepend('<div class="imagen-cards10"><img src="/imagenesjs/contenedor25.jpg" alt=""></div>');
+$("#cards2").prepend('<div class="imagen-cards10"><img src="../imagenesjs/contenedor25.jpg" alt=""></div>');
 $(".imagen-cards10").hide(); 
 $(".imagen-cards10").delay(1); 
 $(".imagen-cards10").slideDown(19000); 
 
 /* LOGO */
 
-$("#cards").prepend('<div class="imagen-logosar"><img src="/imagenesjs/Lambo-Logo.png" alt=""></div>');
+$("#cards").prepend('<div class="imagen-logosar"><img src="../imagenesjs/Lambo-Logo.png" alt=""></div>');
 
-$("#cards1").prepend('<div class="imagen-logosar1"><img src="/imagenesjs/Lambo-Logo.png" alt=""></div>');
+$("#cards1").prepend('<div class="imagen-logosar1"><img src="../imagenesjs/Lambo-Logo.png" alt=""></div>');
 
-$("#cards2").prepend('<div class="imagen-logosar2"><img src="/imagenesjs/Lambo-Logo.png" alt=""></div>');
+$("#cards2").prepend('<div class="imagen-logosar2"><img src="../imagenesjs/Lambo-Logo.png" alt=""></div>');
 
 
 
